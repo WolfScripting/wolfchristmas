d5517aa3594e42e6f327b750fc2932212d610e83
WolfScripting <74819581+WolfScripting@users.noreply.github.com>
2021-01-04 23:41:21 +0100

    Uploaded Files
    Ive uploaded the files

---
+++ wolfchristmas/__resource.lua
@@ -0,0 +1,4 @@
+client_script "script.lua"
+
+--THIS SCRIPT BELONGS TO Ocuro#0001 or ocuro@ocuro.de!
+--DO NOT COPY OR REUPLOAD WITHOUT MY PERMISSION!
---
+++ wolfchristmas/script.lua
@@ -0,0 +1,17 @@
+Citizen.CreateThread(function()
+    while true 
+    	do
+		
+		SetWeatherTypePersist("XMAS")
+        SetWeatherTypeNowPersist("XMAS")
+        SetWeatherTypeNow("XMAS")
+        SetOverrideWeather("XMAS")
+    	
+		Citizen.Wait(1)
+	end
+
+end)
+
+
+--THIS SCRIPT BELONGS TO Ocuro#0001 or ocuro@ocuro.de!
+--DO NOT COPY OR REUPLOAD WITHOUT MY PERMISSION!
